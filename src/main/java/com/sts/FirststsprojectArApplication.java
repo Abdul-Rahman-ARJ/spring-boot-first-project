53dcffc5c6888e3de4600c97448eb9e1955af467
Abdul Rahman <Abdul.Rahman@eton-solutions.com>
2023-02-21 20:07:18 +0530

    first commit

---
+++ src/main/java/com/sts/FirststsprojectArApplication.java
@@ -0,0 +1,12 @@
+package com.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirststsprojectArApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FirststsprojectArApplication.class, args);
+		System.out.print("Starting Web App........");
+	}
+}
